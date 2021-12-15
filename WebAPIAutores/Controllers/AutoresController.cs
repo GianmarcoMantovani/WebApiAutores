13bd8f472c071ff66118812702b1a9446dc44c36
GianmarcoMantovani <mantovanigianm@gmail.com>
2021-12-15 12:12:44 -0300

    .

--- WebAPIAutores/Controllers/AutoresController.cs
+++ WebAPIAutores/Controllers/AutoresController.cs
@@ -31,37 +31,16 @@ namespace WebAPIAutores.Controllers
             this.authorizationService = authorizationService;
             //this.configuration = configuration;
         }
-       
+
         [HttpGet(Name = "obtenerAutores")] // api/autores
         [AllowAnonymous]
-        public async Task<IActionResult> Get([FromQuery] bool incluirHATEOAS = true)
+        [ServiceFilter(typeof(HATEOASAutorFilterAttribute))]
+        public async Task<ActionResult<List<AutorDTO>>> Get([FromHeader] string incluirHATEOAS)
         {
             var autores = await context.Autores.ToListAsync();
-            var dtos = mapper.Map<List<AutorDTO>>(autores);
-            if (incluirHATEOAS)
-            {
-                var esAdmin = await authorizationService.AuthorizeAsync(User, "esAdmin");
-
-                //dtos.ForEach(dto => GenerarEnlaces(dto, esAdmin.Succeeded));
-
-                var resultado = new ColeccionDeRecursos<AutorDTO> { Valores = dtos };
-                resultado.Enlaces.Add(new DatoHATEOAS(
-                    enlace: Url.Link("obtenerAutores", new { }),
-                    descripcion: "self",
-                    metodo: "GET"));
-
-                if (esAdmin.Succeeded)
-                {
-                    resultado.Enlaces.Add(new DatoHATEOAS(
-                        enlace: Url.Link("crearAutor", new { }),
-                        descripcion: "crear-autor",
-                        metodo: "POST"));
-                }
-                return Ok(resultado);
-            }
-
-            return Ok(dtos);
+            return mapper.Map<List<AutorDTO>>(autores);
         }
+
 
         [HttpGet("{id:int}", Name = "obtenerAutor")]
         [AllowAnonymous]
@@ -84,7 +63,7 @@ namespace WebAPIAutores.Controllers
         }
 
         [HttpGet("{nombre}", Name = "obtenerAutorPorNombre")]
-        public async Task<ActionResult<List<AutorDTO>>> Get([FromRoute] string nombre)
+        public async Task<ActionResult<List<AutorDTO>>> GetPorNombre([FromRoute] string nombre)
         {
             var autores = await context.Autores.Where(autorBD => autorBD.Nombre.Contains(nombre)).ToListAsync();
 
--- WebAPIAutores/Utilidades/HATEOASAutorFilterAttribute.cs
+++ WebAPIAutores/Utilidades/HATEOASAutorFilterAttribute.cs
@@ -32,9 +32,23 @@ namespace WebAPIAutores.Utilidades
 
             var resultado = context.Result as ObjectResult; //Tomamos el valor de object result
 
-             var modelo = resultado.Value as AutorDTO ?? throw new
-                 ArgumentNullException("Se esperaba una instancia de AutorDTO"); //Tomamos el valor del AutorDTO y si es nulo devolvemos error
-             await generadorEnlaces.GenerarEnlaces(modelo); //Generamos los enlaces del AutorDTO
+            /* var modelo = resultado.Value as AutorDTO ?? throw new
+                 ArgumentNullException("Se esperaba una instancia de AutorDTO"); //Tomamos el valor del AutorDTO y si es nulo devolvemos error*/
+            var autorDTO = resultado.Value as AutorDTO;
+            if(autorDTO == null)
+            {
+                var autoresDTO = resultado.Value as List<AutorDTO> ?? throw new
+                    ArgumentException("Se esperaba una instancia de AutorDTO o List<AutorDTO>");
+
+                autoresDTO.ForEach(async autor => await generadorEnlaces.GenerarEnlaces(autor)); 
+                resultado.Value = autoresDTO;
+                //Si es nulo devolvemos que se esperaba una instancia y le damos a resultado los enlaces de autorDTO
+            }
+            else
+            {
+                await generadorEnlaces.GenerarEnlaces(autorDTO); //Generamos los enlaces del AutorDTO
+            }
+             
 
              await next(); //Le indicamos al siguiente filtro que siga
 
